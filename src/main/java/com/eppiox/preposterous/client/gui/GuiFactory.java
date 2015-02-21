11c2edf6445915092d580604e05439de7a610b55
Mark Epifano <eppiox@gmail.com>
2015-02-21 14:27:11 +1100

    first commit

---
+++ src/main/java/com/eppiox/preposterous/client/gui/GuiFactory.java
@@ -0,0 +1,32 @@
+package com.eppiox.preposterous.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+import java.util.Set;
+
+/**
+ * Created by Epi on 19/02/2015.
+ */
+public class GuiFactory implements IModGuiFactory {
+    @Override
+    public void initialize(Minecraft minecraftInstance)
+    {
+    }
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass()
+    {
+        return ModGuiConfig.class;
+    }
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
+    {
+        return null;
+    }
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
+    {
+        return null;
+    }
+}
---
+++ src/main/java/com/eppiox/preposterous/proxy/ServerProxy.java
@@ -0,0 +1,7 @@
+package com.eppiox.preposterous.proxy;
+
+/**
+ * Created by Epi on 19/02/2015.
+ */
+public class ServerProxy extends CommonProxy {
+}
